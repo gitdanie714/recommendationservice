169bff2b96a26bab803e0312fa33d920e731fd7d
Orji Daniella Amarachi <danielamarachi20@gmail.com>
2025-05-30 07:16:44 -0700

    corrected CORS Origin port

--- src/main/java/com/tsola2002/recommendationservice/controller/RecommendationController.java
+++ src/main/java/com/tsola2002/recommendationservice/controller/RecommendationController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/bookrecommendations")
-@CrossOrigin(origins = "http://localhost:7003")
+@CrossOrigin(origins = "http://localhost:5173")
 public class RecommendationController {
 
 
